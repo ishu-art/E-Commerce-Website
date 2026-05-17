b15d9f4b96c59803cb20365c45bfd944b481fe44
ishu-art <yishu0271@gmail.com>
2026-05-17 20:15:47 +0530

    Update script.js

--- html/script.js
+++ html/script.js
@@ -31,3 +31,15 @@ var MenuItems=document.getElementById("MenuItems");
                 MenuItems.style.maxHeight="0px";
             }
         }
+// cart.html
+var MenuItems=document.getElementById("MenuItems");
+
+        MenuItems.style.maxHeight="0px";
+        function menutoggle(){
+            if(MenuItems.style.maxHeight=="0px"){
+                MenuItems.style.maxHeight="200px";
+            }
+            else{
+                MenuItems.style.maxHeight="0px";
+            }
+        }
