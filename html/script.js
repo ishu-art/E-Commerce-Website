e2dbc49e2fa5d29eda4391b057ef44472934e884
ishu-art <yishu0271@gmail.com>
2026-05-17 20:16:26 +0530

    Update script.js

--- html/script.js
+++ html/script.js
@@ -43,3 +43,18 @@ var MenuItems=document.getElementById("MenuItems");
                 MenuItems.style.maxHeight="0px";
             }
         }
+// account.html
+ var LoginForm=document.getElementById("LoginForm");
+        var RegForm=document.getElementById("RegForm");
+        var Indicator=document.getElementById("Indicator");
+        function register(){
+            RegForm.style.transform="translateX(0px)";
+            LoginForm.style.transform="translateX(0px)";
+            Indicator.style.transform="translateX(100px)";
+        }
+         function login(){
+            RegForm.style.transform="translateX(300px)";
+            LoginForm.style.transform="translateX(300px)";
+            Indicator.style.transform="translateX(0px)";
+            
+        }
