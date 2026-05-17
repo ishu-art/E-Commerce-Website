76980928d601ac886b2949e0682956d8e2160740
ishu-art <yishu0271@gmail.com>
2026-05-17 20:12:56 +0530

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,11 +1,22 @@
- var MenuItems=document.getElementById("MenuItems");
+// Product image change code
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("Small-img");
 
-        MenuItems.style.maxHeight="0px";
-        function menutoggle(){
-            if(MenuItems.style.maxHeight=="0px"){
-                MenuItems.style.maxHeight="200px";
-            }
-            else{
-                MenuItems.style.maxHeight="0px";
-            }
-        }
+for (let i = 0; i < SmallImg.length; i++) {
+    SmallImg[i].onclick = function () {
+        ProductImg.src = this.src;
+    };
+}
+
+// Menu toggle code
+var MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px";
+    } else {
+        MenuItems.style.maxHeight = "0px";
+    }
+}
